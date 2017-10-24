0110ed62f377a37936baceb654ef02345c9bb3c0
Jose Tapia <jose.rikleo.tapia@gmail.com>
2017-10-24 22:52:19 +0200

    Advanced Lists

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { DishProvider} from './../../providers/dish/dish';
+import { FavoriteProvider} from './../../providers/favorite/favorite';
 import { DishdetailPage } from './../dishdetail/dishdetail';
 /**
  * Generated class for the MenuPage page.
@@ -23,6 +24,7 @@ export class MenuPage implements OnInit {
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     private dishservice: DishProvider,
+    private favoriteservice: FavoriteProvider,
     @Inject('BaseURL') private BaseURL ) {
   }
 
@@ -32,14 +34,19 @@ export class MenuPage implements OnInit {
         errmess => this.errMess = <any>errmess);
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MenuPage');
+  }
+
   dishSelected(event, dish) {
     this.navCtrl.push(DishdetailPage, {
       dish: dish
     });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MenuPage');
+  addToFavorites(dish: Dish) {
+    console.log('Adding to Favorites', dish.id);
+    this.favoriteservice.addFavorite(dish.id);
   }
 
 }
