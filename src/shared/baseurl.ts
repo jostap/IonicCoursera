78bcf73106263b9855af5e9c2535fe5d2ccdf387
Jose Tapia <jose.tapia@ki.se>
2018-03-22 12:58:59 +0100

    Test Push Notifications

--- src/shared/baseurl.ts
+++ src/shared/baseurl.ts
@@ -1,2 +1,9 @@
-export const baseURL = 'http://localhost:3000/';
+//export const baseURL = 'http://localhost:3000/';
+
+// Linux at home
 //export const baseURL = 'http://192.168.1.68:3000/';
+
+// Mac
+//export const baseURL = 'http://130.229.13.171:3000/';
+export const baseURL = 'http://130.229.5.92:3000/';
+
